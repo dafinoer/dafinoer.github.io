c62daaae74ee26dfadab76311f5e511d7390cb70
dafinoer <noerdafi@gmail.com>
2018-04-11 16:50:19 +0700

    add age

--- js/base.js
+++ js/base.js
@@ -1,6 +1,30 @@
 var myVuew = new Vue({
     el:'#test',
     data:{
-        date_time: new Date().getSeconds()
+        date_time: 0
+    },
+    methods:{
+        umur:function(){
+            setInterval(()=>  this.count(), 1000);
+        },
+        count:function(){
+            var data = new Date();
+            var myAge = new Date(1993, (1 - 1), 13);
+            
+            var tot = data - myAge;
+
+            console.log(myAge.getUTCDay());
+
+            var satuan = Math.floor(tot / (1000*60*60*24*365));
+            console.log(satuan);
+
+            this.date_time = satuan
+        }
+    },
+    computed:{
+        tanggal:function(){
+            this.count()
+            return this.date_time;
+        }
     }
 })
