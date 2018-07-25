21518d98b49d400f3f7be93fe1419f2db4bfd174
dafinoer <dafidzeko@gmail.com>
2018-07-25 16:03:23 +0700

    fix typo

--- js/typing.js
+++ js/typing.js
@@ -1,5 +1,4 @@
 
-//<span>quote from joker</span> ^1000\n `<span>If you’re good at something, never do it for free.</span>` ^1000\n `it’s not about the money, it’s about sending a message.` ^1000\n `<span class="username-name">user@local:~$</span>`
 var typed = new Typed('#holla', {
     strings:['Hallo World', "My Name Dafi", "Nice To Meet You"],
     typeSpeed: 90,
@@ -17,7 +16,7 @@ var typed = new Typed('#holla', {
 })
 
 var subTitle = new Typed('#sub-title', {
-    strings:['Software Developer', "Outdoor Life Enthusias", "Software Developer | Outdoor Life Enthusias"],
+    strings:['Software Developer', "Outdoor Life Enthusiast", "Software Developer | Outdoor Life Enthusiast"],
     typeSpeed: 90,
     startDelay: 10,
     loop:false,
